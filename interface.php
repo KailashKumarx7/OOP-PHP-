a539ef7441b1893f3f301a43f79e1e19a5f49784
Kailash Kumar <91149321+KailashKumarx7@users.noreply.github.com>
2022-08-15 14:29:08 +0545

    adding interface class practice

---
+++ interface.php
@@ -0,0 +1,34 @@
+<?php
+
+interface parent1{
+
+   function calc($a, $b);
+}
+
+
+interface parent2{
+    function sub($c, $d);
+}
+
+
+class childClass implements parent1, parent2{
+
+public function calc($a, $b){
+    echo $a*$b;
+}
+
+public function sub($c, $d){
+    echo $c-$d;
+}
+
+
+
+}
+
+$test = new childClass();
+$test->calc(5,10);
+echo "\n";
+$test->sub(44,10);
+
+
+?>

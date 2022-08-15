5aa598d995ad48cd5a9f88caf1733d4d331a5e8e
Kailash Kumar <91149321+KailashKumarx7@users.noreply.github.com>
2022-08-15 13:29:54 +0545

    Accessmodefier

---
+++ Access_Modifiers.php
@@ -0,0 +1,27 @@
+<?php
+
+
+class base{
+    protected $name;
+    
+    public function __construct($n){
+        $this->name = $n;
+    }
+ protected function show(){
+        echo "Your Name :" .$this->name. "<br>";
+    }
+ 
+}
+
+class derived extends base{
+    public function get(){
+        echo "Your Name :" .$this->name. "<br>";
+    }
+}
+
+$test = new derived("Kailash");
+// $test->name="kishor";
+$test->get();
+
+
+?>

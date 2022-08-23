a77d511c11a8c833f2336bec9accd657271b2b5c
Kailash Kumar <91149321+KailashKumarx7@users.noreply.github.com>
2022-08-23 22:00:40 +0545

    mysqli object oriented implementation

--- mysqli_object_oriented.php
+++ mysqli_object_oriented.php
@@ -30,6 +30,30 @@ mysqli_error()                      $conn->error();
 mysqli_error_list()              $conn->error_list();
 */
 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "testing";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if($conn->connect_error){
+    die("Connection Failed: ".$conn->connect_error);
+}
+
+$sql = "SELECT * FROM students";
+$result = $conn->query($sql);
+
+
+if($result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+        echo "Id: {$row["id"]} - Name: {$row["student_name"]} - Age: {$row["age"]}\n";
+    }
+}else{
+        echo "No result found.";
+    }
+
+$conn->close();
 
 
 

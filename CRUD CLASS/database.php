e9f21e3cbc00b8908e90356b33522abf7c57dc4e
Kailash Kumar <91149321+KailashKumarx7@users.noreply.github.com>
2022-08-27 19:24:59 +0545

    push

---
+++ CRUD CLASS/database.php
@@ -0,0 +1,64 @@
+<?php
+
+class Database{
+
+    private $db_host = "localhost";
+    private $db_user = "root";
+    private $db_pass = "";
+    private $db_name = "test";
+
+    private $mysqli = "";
+    private $result = array();
+    private $conn = false;
+
+
+
+    public function __construct(){
+
+        if(!$this->conn){
+            $this->mysqli = new mysqli($this->db_host,$this->db_user,$this->db_pass,$this->db_name);
+
+            if($this->mysqli->connect_error){
+                array_push($this->result, $this->mysqli->connect_error);
+                return false;
+            }
+        }
+
+
+    }
+
+    //Function to insert into the database
+    public function insert(){
+
+    }
+
+
+    //Function to update row in database
+    public function update(){
+
+    }
+
+    //Function to delete table or row(s) from database
+    public function delete(){
+
+    }
+
+
+    //Function to Select from the database
+    public function select(){
+
+    }
+
+
+    //close connection
+    public function --destruct(){
+
+    }
+
+
+
+
+}
+
+
+?>

21c68af802904a70ea323984aa9fd745ae6bb791
Kailash Kumar <91149321+KailashKumarx7@users.noreply.github.com>
2022-08-28 19:39:29 +0545

    update function complete

--- CRUD CLASS/index.php
+++ CRUD CLASS/index.php
@@ -5,8 +5,14 @@
 
  
  $obj = new Database();
- $obj->insert('student', ['student_name'=>'kp suri', 'age'=>'30', 'city'=>'vellor']);
- echo "Insert reslut is: ";
+//  $obj->insert('student', ['student_name'=>'kp suri', 'age'=>'30', 'city'=>'vellor']);
+//  echo "Insert reslut is: ";
+//  print_r($obj->getResult());
+ 
+
+
+$obj->update('student', ['student_name'=>'kp bazeee', 'age'=>'100', 'city'=>'vellor'], 'id="2"');
+ echo "update reslut is: ";
  print_r($obj->getResult());
  
  ?>

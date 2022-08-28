21a7c93716abbebd451b0a927047955cf16edd22
Kailash Kumar <91149321+KailashKumarx7@users.noreply.github.com>
2022-08-28 18:37:35 +0545

    modefy

--- CRUD CLASS/index.php
+++ CRUD CLASS/index.php
@@ -5,7 +5,7 @@
 
  
  $obj = new Database();
- $obj->insert('student', ['id'=>'5', student_name'=>'anubhav suri', 'age'=>'20', 'city'=>'bolawabaad']);
+ $obj->insert('student', ['id'=>'5', 'student_name'=>'anubhav suri', 'age'=>'20', 'city'=>'bolawabaad']);
  
  
  ?>

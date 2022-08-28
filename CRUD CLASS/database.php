652bb421f098fe2d1bd2a01bdd439e8267a065de
Kailash Kumar <91149321+KailashKumarx7@users.noreply.github.com>
2022-08-28 20:03:51 +0545

    delete function complete

--- CRUD CLASS/database.php
+++ CRUD CLASS/database.php
@@ -88,7 +88,28 @@ class Database{
     }
 
     //Function to delete table or row(s) from database
-    public function delete(){
+    public function delete($table, $where= null){
+        if($this->tableExists($table)){
+
+            $sql = "DELETE FROM $table ";
+            if($where!= null){
+                $sql .= "WHERE $where";
+            }
+            if($this->mysqli->query($sql)){
+                array_push($this->result, $this->mysqli->affected_rows);
+                return true;
+    
+    
+               }else{
+                array_push($this->result, $this->mysqli->error);
+    
+               }
+            
+
+        }
+        else{
+            return false;
+        }
 
     }
 
--- CRUD CLASS/index.php
+++ CRUD CLASS/index.php
@@ -11,8 +11,15 @@
  
 
 
-$obj->update('student', ['student_name'=>'kp bazeee', 'age'=>'100', 'city'=>'vellor'], 'id="2"');
- echo "update reslut is: ";
- print_r($obj->getResult());
+// $obj->update('student', ['student_name'=>'kp bazeee', 'age'=>'100', 'city'=>'vellor'], 'id="2"');
+//  echo "update reslut is: ";
+//  print_r($obj->getResult());
+
+
+$obj->delete('student','id="3"');// table name and condition
+//  echo "delete reslut is: ";
+//  print_r($obj->getResult());
+
+
  
  ?>

843b05f8ebc7062207f474fecd7ab7b9f1107215
Kailash Kumar <91149321+KailashKumarx7@users.noreply.github.com>
2022-08-30 23:13:17 +0545

    commit

--- CRUD CLASS/database.php
+++ CRUD CLASS/database.php
@@ -115,14 +115,14 @@ class Database{
 
 
     //Function to Select from the database
-    public function select($table, $rows="*", $join = null, $where=null,$order=null$limit=null){
+    public function select($table, $rows="*", $join = null, $where=null,$order=null,$limit=null){
 
         if($this->tableExists($table)){
 
             $sql = "SELECT $rows FROM $table";
             if($join !=null){
-                $sql ..="JOIN $join";
-        
+                $sql .="JOIN $join";
+         
             }
 
             if($where !=null){
@@ -138,6 +138,18 @@ class Database{
             }
 
             echo $sql;
+
+            $query =$this->mysqli->query($sql);
+
+            if($query){
+                $this->result = $query->fetch_all(MYSQLI_ASSOC);
+                return true;
+            }else{
+                array_push($this->result, $this->mysqli->error);
+                return false;
+            }
+
+            
         }else{
 
         }

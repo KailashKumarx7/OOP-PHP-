1398902660fcfbdbdc85dc0505b8282bc61c9b3e
Kailash Kumar <91149321+KailashKumarx7@users.noreply.github.com>
2022-08-30 22:55:36 +0545

    commit

--- CRUD CLASS/database.php
+++ CRUD CLASS/database.php
@@ -117,6 +117,24 @@ class Database{
     //Function to Select from the database
     public function select(){
 
+
+
+
+
+    }
+
+    public function sql($sql){
+        $query = $this->mysqli->query($sql);
+
+        if($query){
+            $this->result = $query->fetch_all(MYSQLI_ASSOC);
+            return true;
+        }else{
+            array_push($this->result, $this->mysqli->error);
+            return false;
+        }
+
+
     }
 
 

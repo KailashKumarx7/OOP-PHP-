17bc3e6a4a6471617756e417a431155041e49d13
Kailash Kumar <91149321+KailashKumarx7@users.noreply.github.com>
2022-08-30 23:05:33 +0545

    commit

--- CRUD CLASS/database.php
+++ CRUD CLASS/database.php
@@ -115,7 +115,33 @@ class Database{
 
 
     //Function to Select from the database
-    public function select(){
+    public function select($table, $rows="*", $join = null, $where=null,$order=null$limit=null){
+
+        if($this->tableExists($table)){
+
+            $sql = "SELECT $rows FROM $table";
+            if($join !=null){
+                $sql ..="JOIN $join";
+        
+            }
+
+            if($where !=null){
+                $sql .= "WHERE $where";
+            }
+
+            if($order !=null){
+                $sql .= "ORDER BY $order";
+            }
+
+            if($limit !=null){
+                $sql .= "LIMIT 0, $limit";
+            }
+
+            echo $sql;
+        }else{
+
+        }
+
 
 
 

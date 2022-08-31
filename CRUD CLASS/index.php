5b5c7bcdca85a5f1c383b74a7ee96cb2d6365baa
Kailash Kumar <91149321+KailashKumarx7@users.noreply.github.com>
2022-08-31 21:53:34 +0545

    commit

--- CRUD CLASS/index.php
+++ CRUD CLASS/index.php
@@ -5,7 +5,7 @@
 
                              
  $obj = new Database();
- $obj->insert('student', ['student_name'=>'Sagar', 'age'=>'20', 'city'=>'MNR']);
+ $obj->insert('student', ['student_name'=>'Bikash sharma', 'age'=>'22', 'city'=>'Dhangadi']);
  echo "Insert reslut is: ";
  print_r($obj->getResult());
  
